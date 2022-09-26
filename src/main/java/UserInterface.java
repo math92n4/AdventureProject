da33565fd89ede5538a7ac73258cc7afdeb8e78a
math92n4 <math92n4@stud.kea.dk>
2022-09-26 15:28:06 +0200

    brugeroverflade uden rooms

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
+
 public class UserInterface {
+
+    Scanner s = new Scanner(System.in);
+    public void start() {
+
+        System.out.println("WELCOME TO THE ADVENTURE GAME");
+        System.out.println("-----------------------------");
+        System.out.println("Type 'go north' to go north");
+        System.out.println("Type 'go south' to go south");
+        System.out.println("Type 'go west' to go west");
+        System.out.println("Type 'go east' to go east");
+        System.out.println("Type 'look around' to look around");
+
+        do {
+
+
+            String walk = s.nextLine();
+
+            switch (walk) {
+                case "go north":
+                    System.out.println("You've gone north");
+                    break;
+                case "go south":
+                    System.out.println("You've gone south");
+                    break;
+                case "go west":
+                    System.out.println("You've gone west");
+                    break;
+                case "go east":
+                    System.out.println("You've gone east");
+                    break;
+                case "look around":
+                    System.out.println("Looking around");
+                    break;
+            }
+        } while (true);
+
+
+
+
+    }
+
+
 }
